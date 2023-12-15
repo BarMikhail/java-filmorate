1129b39eb0090e78feb9f7e359a9a9bfc3403a49
М.Баранов <m.baranov@rlt.ru>
2023-12-15 23:47:11 +0300

    Проверка ТЗ 11

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -38,12 +38,12 @@ public class User {
         friends.remove(id);
     }
 
-    public Map<String, Object>  toMap(){
+    public Map<String, Object> toMap() {
         Map<String, Object> values = new HashMap<>();
-        values.put("email",email);
-        values.put("login",login);
-        values.put("name",name);
-        values.put("birthday",birthday);
+        values.put("email", email);
+        values.put("login", login);
+        values.put("name", name);
+        values.put("birthday", birthday);
         return values;
     }
 }
